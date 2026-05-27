c145345198dfc0e34957c379bd39ea1b4261f1ca
Jayanth-0407 <jayanth04072006@gmail.com>
2026-05-27 15:12:53 +0530

    Update pii_scrubber.py

--- pii_scrubber.py
+++ pii_scrubber.py
@@ -1,8 +1,21 @@
-from presidio_analyzer import AnalyzerEngine
+from presidio_analyzer import AnalyzerEngine, PatternRecognizer, Pattern
 from presidio_anonymizer import AnonymizerEngine
 
-# Initialize engines once at module load to prevent latency on every API call
 analyzer = AnalyzerEngine()
+
+imps_pattern = Pattern(name="imps_name", regex=r"(?<=IMPS-TRANSFER-\d{10}-)[A-Z\s]+", score=0.9)
+custom_recognizer = PatternRecognizer(supported_entity="PERSON", patterns=[imps_pattern])
+analyzer.registry.add_recognizer(custom_recognizer)
+
+
+upi_pattern = Pattern(name="upi_pattern", regex=r"[\w.\-_]+@[a-zA-Z]+", score=0.95)
+upi_recognizer = PatternRecognizer(supported_entity="UPI_ID", patterns=[upi_pattern])
+analyzer.registry.add_recognizer(upi_recognizer)
+
+account_pattern = Pattern(name="bank_account", regex=r"\b\d{14}\b", score=0.9)
+account_recognizer = PatternRecognizer(supported_entity="BANK_ACCOUNT", patterns=[account_pattern])
+analyzer.registry.add_recognizer(account_recognizer)
+
 anonymizer = AnonymizerEngine()
 
 def scrub_text(text: str) -> str:
@@ -13,10 +26,10 @@ def scrub_text(text: str) -> str:
         return text
         
     # Analyze text for common PII (Names, Phones, Emails, Crypto, etc.)
-    # Note: You can easily add custom pattern recognizers here for Indian PAN/Aadhaar later
+    
     results = analyzer.analyze(
         text=text, 
-        entities=["PERSON", "EMAIL_ADDRESS", "PHONE_NUMBER", "CRYPTO", "CREDIT_CARD"], 
+        entities=["PERSON", "EMAIL_ADDRESS", "PHONE_NUMBER", "CRYPTO", "CREDIT_CARD","UPI_ID","BANK_ACCOUNT"], 
         language='en'
     )
     
@@ -29,7 +42,7 @@ def clean_transactions(transactions: list) -> list:
     """
     Iterates through transactions and scrubs sensitive text fields.
     """
-    scrubbed_transactions = []
+    scrubbed_transactions = []  
     
     for txn in transactions:
         clean_txn = txn.copy()
